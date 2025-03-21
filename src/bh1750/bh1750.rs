361c41dba2c0aa1c17545a70941f7964ccfefe26
Otto Korhonen <otto@xd.fi>
2025-03-21 14:39:42 +0200

    enhancements in the code

--- src/bh1750/bh1750.rs
+++ src/bh1750/bh1750.rs
@@ -3,25 +3,29 @@ use crate::bh1750::Command;
 use core::borrow::BorrowMut;
 use core::error::Error;
 use core::fmt;
+use embedded_hal::{delay::DelayNs, i2c::SevenBitAddress};
 
-pub struct Bh1750< I2C, E> {
-    pub i2c: I2C,
-    pub address: u8,
+pub struct Bh1750< I2C, E, D> {
+    i2c: I2C,
+    address: SevenBitAddress,
+    delay: D, 
     _error: core::marker::PhantomData<E>,
 }
 
 impl<E: fmt::Debug> Error for Bh1750Error<E> {}
 
-impl<I2C, E> Bh1750< I2C, E>
+impl<I2C, E, D> Bh1750< I2C, E, D>
 where
     I2C: embedded_hal::i2c::I2c<Error = E>,
     E: fmt::Debug,
+    D: DelayNs
 {
 
-    pub fn new(i2c: I2C, address: u8) -> Self {
+    pub fn new(i2c: I2C, address: u8, delay: D) -> Self {
         Self {
             i2c,
             address,
+            delay,
             _error: core::marker::PhantomData,
         }
     }
@@ -29,9 +33,10 @@ where
     pub fn read(&mut self, command: Command) -> Result<u16, Bh1750Error<E>> {
         let mut result = [0u8; 2];
 
-        self.i2c.borrow_mut()
-            .write_read(self.address, &[command as u8], &mut result)
-            .map_err(Bh1750Error::I2cError)?;
+        self.i2c.write_read(self.address, &[command as u8], &mut result).map_err(Bh1750Error::I2cError)?;
+
+        self.delay.delay_ms(120);
+
         Ok(u16::from_be_bytes(result))
     }
 
